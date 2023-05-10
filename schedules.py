741c4fad03b6fbfec03bbc286b0b0d097e038fcd
Filipe Alano Fortunato <62904031+filipealft@users.noreply.github.com>
2023-05-10 10:46:10 -0300

    Update schedules.py

--- schedules.py
+++ schedules.py
@@ -1,6 +1,7 @@
 import schedule
 import threading
 import time
+import subprocess
 from reports import send_daily_report, connect_to_database, get_last_trade
 
 def run_daily_report():
@@ -15,9 +16,13 @@ def run_get_last_trade():
 #     subprocess.run(["python", "arquivo2.py"])
 
 
-schedule.every(5).seconds.do(run_daily_report)
+# schedule.every(5).seconds.do(run_daily_report)
 # schedule.every().day.at("18:00").do(run_daily_report)
 # schedule.every(10).seconds.do(run_file2)
+
+# def run_daily_report():
+#     subprocess.run(["python3", "daily_report.py"])
+
 
 # Iniciar a função de get_last_trade em uma nova thread
 thread = threading.Thread(target=run_get_last_trade)
